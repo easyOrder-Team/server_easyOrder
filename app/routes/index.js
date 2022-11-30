ca0c5fb5a631752fa029c9f213ef66e040175420
ortizdiaz18 <mecatronicaeam2015@gmail.com>
2022-11-30 11:46:04 -0500

    Estructura inicial, BackEnd

---
+++ app/routes/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const router = express.Router()
+const fs = require('fs')
+
+const pathRouter = `${__dirname}`
+
+const removeExtension = (fileName) => {
+    return fileName.split('.').shift()
+}
+
+fs.readdirSync(pathRouter).filter((file) => {
+    const withOutExt = removeExtension(file)
+    const skipe = ['index'].includes(withOutExt)
+    if (!skipe) {
+        router.use(`/${withOutExt}`, require(`${withOutExt}`))
+        console.log('---->',withOutExt )
+
+    }
+})
+
+router.get("*", (req, res)=>{
+    res.status(404).send({Error:'Not Found'})
+})
+
+module.exports = router
---
+++ config/bd.js
@@ -0,0 +1,3 @@
+/**Toda la conexion a la base de datos debera ir en este archivo
+ * no importa el freamwork, orm o aplicación que utilicemos
+ */
