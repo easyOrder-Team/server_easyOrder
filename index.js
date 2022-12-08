9eb55d54125d2db60652838489c973dca3263cc7
Lila Bergamini <lila.bergamini@hotmail.com>
2022-12-08 18:40:52 -0300

    auth0 config

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const router = require('./app/routes')
-const PORT= process.env.PORT || 3000
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const router = require("./app/routes");
+const PORT = process.env.PORT || 3000;
+const { auth } = require("express-openid-connect");
 
-app.use(cors())
-app.use(express.json())
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASEURL,
+  clientID: process.env.CLIENTID,
+  issuerBaseURL: process.env.ISSUER,
+};
 
-app.use('/api/v1', router)
+app.use(cors());
+app.use(express.json());
 
-app.listen(PORT, () =>{
-    console.log(`Listen in ${PORT}`)
-})
+app.use("/api/v1", router);
+app.use(auth(config));
 
+app.listen(PORT, () => {
+  console.log(`Listen in ${PORT}`);
+});
