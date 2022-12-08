625e85d75cbfad87d0d449b260f89395f0daf3bc
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2022-12-08 15:34:01 -0600

    Cambios

--- config/bd.js
+++ config/bd.js
@@ -3,11 +3,7 @@ const { Pool } = require("pg");
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  port:DB_PORT
+ 
 });
 
 module.exports = pool
