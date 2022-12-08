4c9e2346abbce074b52f0a2ce12bee1e5a3c64e9
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2022-12-08 15:39:26 -0600

    dale lila copia

--- config/bd.js
+++ config/bd.js
@@ -3,11 +3,16 @@ const { Pool } = require("pg");
 const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT } = process.env;
 
 const pool = new Pool({
-  user: PGUSER,
-  host: PGHOST,
-  password: PGPASSWORD,
-  database: PGDATABASE,
-  port:PGPORT
+  // user: PGUSER,
+  // host: PGHOST,
+  // password: PGPASSWORD,
+  // database: PGDATABASE,
+  // port:PGPORT
+  user: DB_USER,
+  host: DB_HOST,
+  password: DB_PASSWORD,
+  database: DB_NAME,
+  port: DB_PORT
 });
 
 module.exports = pool
