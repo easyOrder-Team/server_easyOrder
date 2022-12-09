f2a539032fc75dbe373fc3597fa1b108b2083fa9
guillermolarrea <guillermolarrea55@gmail.com>
2022-12-09 09:37:04 -0300

    rutas de disable, active y buscar todos los disables

--- app/controllers/profile.js
+++ app/controllers/profile.js
@@ -1,5 +1,14 @@
 const pool = require("../../config/bd");
 
+const createProfile = async (req, res) => {
+  const {Id_profile, name, lastname, phone, email} = req.body
+  try {
+      await pool.query(`INSERT INTO profile(Id_profile, name, lastname, phone, email, client) VALUES ('${Id_profile}','${name}', '${lastname}', '${phone}','${email}', true )`)
+      res.sendStatus(201)
+  } catch (error) {
+      res.status(404).json({ error: error.message });
+  }
+}
 
 
 
@@ -67,151 +76,46 @@ const pool = require("../../config/bd");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const getDisablesUser = async (req, res) => {
+const deleteUser = async (req, res) => {
+  const {id} = req.params
     try {
-      const dbData = await pool.query(
-        `select profile.id_profile, profile.name, profile.lastname, profile.phone, profile.email, profile.state,  from profile`
-        // inner join products_category ON products_category.id_product = products.id_products
-        // inner join category on category.id_category = products_category.id_categorie WHERE stock = False`
+      const dbData = await pool.query(        
+        `UPDATE profile SET state = false WHERE id_profile = ${id}`
       );
-      return res.json(allData);
+      return res.send('User Deleted');
     } catch (error) {
       res.json(error.message);
     }
   }
 
+  const activeUser = async (req, res) => {
+    const {id} = req.params
+      try {
+       await pool.query(        
+          `UPDATE profile SET state = true WHERE id_profile = ${id}`
+        );
+        return res.send('User Actived');
+      } catch (error) {
+        res.json(error.message);
+      }
+    }
+  
+    const getDisablesUser = async (req, res) => {
+      try {
+        const dbData = await pool.query(
+          `select profile.id_profile, profile.name, profile.lastname, profile.email, profile.phone, profile.state from profile
+          where profile.state = false`
+        );
+        return res.json(dbData);
+      } catch (error) {
+        res.json(error.message);
+      }
+    }
+
 
 module.exports = {
-  
+   createProfile,
+   deleteUser,
+   activeUser,
+   getDisablesUser
   };
--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,4 +1,5 @@
-const express = require('express')
+const express = require('express');
+const { createProfile, deleteUser, activeUser, getDisablesUser } = require('../controllers/profile');
 const router = express.Router();
 
 
@@ -7,197 +8,14 @@ const router = express.Router();
 
 
 
+router.post('/', createProfile)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+router.delete('/:id', deleteUser)
+router.put('/:id', activeUser)
+router.get('/disables', getDisablesUser)
 
 
 module.exports = router
