ec58212cc57a04aac21dc30fc07c84519ce40840
guillermolarrea <guillermolarrea55@gmail.com>
2022-12-09 15:09:40 -0300

    profile completo y funcionando

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,32 +1,23 @@
 const express = require("express");
 const router = express.Router();
-
-
-
-
-
-
-
-router.post('/', 
-  createProfile,
-  updateProfile,
-  becomeAdmin,
-  getAllProfile,
-  getProfile,
-)
-
-
-
-
-router.delete("/:id', deleteUser)
-router.put('/:id', activeUser)
-router.get('/disables", getDisablesUser);
-
-
-router.post("/", createProfile);
-router.put("/updateProfile/:id", updateProfile);
-router.put("/:id", becomeAdmin);
-router.get("/", getAllProfile);
-router.get("/:id", getProfile);
-
-module.exports = router;
+const {
+    createProfile,
+    updateProfile,
+    becomeAdmin,
+    getAllProfile,
+    getProfile,
+    getDisablesUser,
+    activeUser,
+    deleteUser,
+  } = require("../controllers/profile");
+  
+  router.post("/", createProfile);
+  router.put("/updateProfile/:id", updateProfile);
+  router.put("/:id", becomeAdmin);
+  router.get("/", getAllProfile);
+  router.get("/:id", getProfile); 
+  router.get("/filter/disables", getDisablesUser);
+  router.delete("/:id", deleteUser);
+  router.put('/active/:id', activeUser);
+  
+  module.exports = router;
--- config/bd.js
+++ config/bd.js
@@ -3,7 +3,6 @@ const { Pool } = require("pg");
 // const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT } = process.env;
 const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 
-const { DB_USER, DB_HOST, DB_PASSWORD, DB_NAME, DB_PORT } = process.env;
 const pool = new Pool({
   user: DB_USER,
   host: DB_HOST,
