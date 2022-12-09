e70aefff23f0ba2a30e8cda3d560f94073c767aa
Lila Bergamini <lila.bergamini@hotmail.com>
2022-12-08 22:38:05 -0300

    etc

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
 const {
   createProfile,
   updateProfile,
@@ -14,12 +13,5 @@ router.put("/updateProfile/:id", updateProfile);
 router.put("/:id", becomeAdmin);
 router.get("/", getAllProfile);
 router.get("/:id", getProfile);
-=======
-const {createProfile, getAllProfile, getProfile} = require('../controllers/profile')
-
-router.post('/', createProfile)
-router.get('/', getAllProfile)
-router.get('/:id', getProfile)
->>>>>>> ab81c56562cf8f54e4f2b5465c134f17f707510c
 
 module.exports = router;
