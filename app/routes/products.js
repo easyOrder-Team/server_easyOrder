cf6d5a9752810e3f87fcb9ae59f8acc7d1184e6a
Miguel Gallardo <miguel.gallardof@icloud.com>
2022-12-10 20:11:00 -0500

    back update

--- app/routes/products.js
+++ app/routes/products.js
@@ -1,31 +1,31 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const { createProduct,
-    getProducts,
-    deleteProduct,
-    getProductById,
-    getCategories,
-    createCategory,
-    filterByCategory,
-    updateProduct,
-    priceOrder,
-    ActiveProduct,
-    getDisablesProducts,
-    timePreparationOrder,} =require ('../controllers/products')
+const {
+  createProduct,
+  getProducts,
+  deleteProduct,
+  getProductById,
+  getCategories,
+  createCategory,
+  filterByCategory,
+  updateProduct,
+  priceOrder,
+  ActiveProduct,
+  getDisablesProducts,
+  timePreparationOrder,
+} = require("../controllers/products");
 
-    
-   
-    router.get("/categories", getCategories);
-    router.get("/disables", getDisablesProducts)
-    router.post("/category", createCategory);
-    router.get("/filter/filterByCategory", filterByCategory);
-    router.put("/update/:id", updateProduct);
-    router.delete("/:id", deleteProduct);
-    router.get("/:id", getProductById);
-    router.post("/", createProduct);
-    router.get("/", getProducts);
-    router.get("/filter/filter/priceOrder", priceOrder)
-    router.get("/filter/filter/timePreparationOrder", timePreparationOrder)
-    router.put('/:id', ActiveProduct) // activa el product
+router.get("/categories", getCategories);
+router.get("/disables", getDisablesProducts);
+router.post("/category", createCategory);
+router.get("/filter/filterByCategory", filterByCategory);
+router.put("/update/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+router.get("/:id", getProductById);
+router.post("/", createProduct);
+router.get("/", getProducts);
+router.get("/filter/filter/priceOrder", priceOrder);
+router.get("/filter/filter/timePreparationOrder", timePreparationOrder);
+router.put("/:id", ActiveProduct); // activa el product
 
-module.exports = router
+module.exports = router;
