93a52892a36638730e46780058b37d7d86bb790e
guillermolarrea <guillermolarrea55@gmail.com>
2022-12-16 20:55:08 -0300

    relacion reservation-site y filtro de precio entre parametros

--- app/controllers/reservation.js
+++ app/controllers/reservation.js
@@ -1,15 +1,60 @@
 const pool = require('../../config/bd');
 
+const orderReservation = (dbData) => {
+  allData = dbData.rows.map((r) => {
+    return {
+      id: r.id_reservation,
+      amount_persons: r.amount_persons,
+      date: r.date,
+      hour: r.hour,
+      state: r.state,
+      id_profile: r.id_profile,
+      id_site: r.id_site,
+      avalible: r.avalible,
+      num_table: [{num_table: r.num_table }],
+    };
+  });
+  let notRepeat = [];
+
+  for (let i = 0; i < allData.length; i++) {
+    if (notRepeat.findIndex((p) => p.id === allData[i].id) === -1)
+      notRepeat.push(allData[i]);
+    else {
+      let index = notRepeat.findIndex((p) => p.id === allData[i].id);
+      notRepeat[index].num_table = [
+        ...notRepeat[index].num_table,
+        ...allData[i].num_table,
+      ];
+    }
+  }
+  return notRepeat;
+};
+
 const createReservation = async (req, res) => {
-  const { Amount_Persons, date, hour, id_profile } = req.body;
+  const { Amount_Persons, date, hour, id_profile, num_table } = req.body;
   try {
+    let allSite = await pool.query(`SELECT * FROM site`)
+    allSite = allSite.rows
+ 
     const reservation = await pool.query(
-      `SELECT * FROM reservation WHERE id_profile = ${id_profile} and date = '${date}'`
+      `SELECT * FROM reservation WHERE id_profile = '${id_profile}' and date = '${date}'`
     );
     if (reservation.rowCount === 0) {
       await pool.query(
-        `INSERT INTO reservation( Amount_Persons, Date, Hour, id_Profile) VALUES ( ${Amount_Persons}, '${date}', '${hour}', ${id_profile})`
+        `INSERT INTO reservation( Amount_Persons, Date, Hour, id_Profile) VALUES ( ${Amount_Persons}, '${date}', '${hour}', '${id_profile}')`
       );
+      let idReservation = await pool.query("SELECT * FROM reservation WHERE id_reservation= (SELECT MAX(id_reservation) FROM reservation);")
+      idReservation = idReservation.rows[0].id_reservation
+      console.log(idReservation)
+      for (let i = 0; i < allSite.length; i++) {
+        for (let j = 0; j < num_table.length; j++) {
+          if (allSite[i].num_table === num_table[j]) {
+            await pool.query(
+              `INSERT INTO reservation_site (id_reservation, id_site) VALUES('${idReservation}','${allSite[i].id_site}' )`
+            );
+          }
+        }
+      }
       return res.send('reservation made successfully');
     } else {
       res.send('you already have a reservation with these details');
@@ -25,7 +70,6 @@ const deleteReservation = async (req, res) => {
     const reservation = await pool.query(
       `UPDATE reservation SET state = false WHERE id_reservation = ${id}`
     );
-    if (reservation.rowCount === 0) throw new Error('Non-existent reservation');
     return res.json('The reservation has been deleted');
   } catch (error) {
     res.json({ message: error.message });
@@ -49,15 +93,21 @@ const getAllDisablesReservation = async (req, res) =>{
 
 const getAllReservation = async (req, res)=>{
     try {
-        const { id } = req.params;
+      const { id } = req.params
+      let allData = []
+      console.log(id)
         let reservations
         if(id){
-            reservations = await pool.query(`SELECT * FROM reservation WHERE id_profile = ${id} and state = true`)
+            reservations = await pool.query(`SELECT * FROM reservation 
+            INNER JOIN reservation_site ON reservation_site.id_reservation = reservation.id_reservation
+            INNER JOIN site ON site.id_site = reservation_site.id_site WHERE reservation.id_profile = '${id}' and state = true
+            `)
         }else{
             reservations = await pool.query(`SELECT * FROM reservation WHERE state = true`)
         }
         if (reservations.rowCount !== 0){
-        res.json(reservations.rows)
+          allData = orderReservation(reservations)
+        res.json(allData)
         }else{
           res.send("There is no active reservation")
         }
