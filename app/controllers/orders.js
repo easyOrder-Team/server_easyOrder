5a0342b1d0487282ef8f6456319dbe4ef47a8778
MiguelA02 <101620100+MiguelA02@users.noreply.github.com>
2023-01-10 21:04:19 -0600

    rutas para el jefe

--- app/controllers/orders.js
+++ app/controllers/orders.js
@@ -178,9 +178,35 @@ const filterOrdersByState = async (req, res)=>{
   }
 }
 
+const getActiveOrders = async(req, res) => {
+  try {
+    orders = await pool.query(`SELECT orders.id_orders, orders.avalible, orders.id_mesa, orders.id_profile, products.id_products, product_order.amount_product, product_order.total_price, products.name, orders.avalible, products.price, products.stock, orders.id_mesa, products.image FROM orders
+      INNER JOIN product_order ON product_order.id_order = orders.id_orders
+      INNER JOIN products ON products.id_products = product_order.id_product
+      INNER JOIN payments ON payments.id_order = orders.id_orders WHERE orders.avalible = true`);
+        
+      orders = orderOrders(orders);
+      res.json(orders)
+      
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+}
+
+const deleteOrder = async (req,res) => {
+  try {
+    const {id} = req.params;
+    await pool.query(`UPDATE orders SET avalible = false WHERE id_orders = ${id}`)
+    res.sendStatus(204)
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+}
 module.exports = { 
   createOrder,
   getAllOrders, 
   changeStateOrder, 
   filterOrdersByState,
-  getOrderById };
+  getOrderById,
+  deleteOrder,
+  getActiveOrders};
